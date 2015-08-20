78c25a74cad79e4959427817d0d7d960805485bf
huyang_XcodeDemo <huyang_XcodeDemo@163.com>
2015-08-20 17:36:00 +0800

    添加Person类

---
+++ XcodeGitDemo/Person.h
@@ -0,0 +1,13 @@
+//
+//  Person.h
+//  XcodeGitDemo
+//
+//  Created by huyang on 15/8/20.
+//  Copyright (c) 2015年 huyang. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Person : NSObject
+
+@end
---
+++ XcodeGitDemo/Person.m
@@ -0,0 +1,13 @@
+//
+//  Person.m
+//  XcodeGitDemo
+//
+//  Created by huyang on 15/8/20.
+//  Copyright (c) 2015年 huyang. All rights reserved.
+//
+
+#import "Person.h"
+
+@implementation Person
+
+@end
